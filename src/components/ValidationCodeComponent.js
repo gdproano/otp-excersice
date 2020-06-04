8df49b28768fa6303731d72f3d4890400bd39993
foqc <fabiaq44@gmail.com>
2020-06-03 21:02:05 -0500

    adding validation styles and inputs

--- src/components/ValidationCodeComponent.js
+++ src/components/ValidationCodeComponent.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useRef, createRef } from 'react';
 import logo from '../assets/images/icon.png';
+import Input from './common/Input';
 const DIGITS = 6
+const FILEDS = new Array(6).fill(1)
 
-const ValidationComponent = ({email = 'exxxxxiz@gmail.com', phone='09xxxxx651'}) => {
+const checkReference = (ref, index) => !!(ref && ref.current && ref.current[index])
+
+const ValidationComponent = ({ email = 'exxxxxiz@gmail.com', phone = '09xxxxx651' }) => {
+
+    const [data, setData] = useState([])
+    const refs = useRef(FILEDS.map(() => createRef()));
+
     return <div className="validation-section">
         <img src={logo} className="validation-icon" alt="logo" />
         <p className="title">Código de validación</p>
         <p className="description">Ingrese el código de {DIGITS} dígitos que hemos enviado a tu correo electrónico {email} y celular {phone}</p>
-        
+        <div className="inputs">
+            {
+                FILEDS.map((_, i) => <Input key={i} inputReference={refs.current[i]} id={i} value={data[i]}
+                    minLength={1} maxLength={1} inputCustomClass="input-secret"
+                    onChange={(_, value) => {
+                        if (checkReference(refs, i + 1) && value !== '')
+                            refs.current[i + 1].current.focus()
+                        data[i] = value
+                        setData([...data])
+                    }}
+                    onPressBack={_ => {
+                        if (checkReference(refs, i - 1)) refs.current[i - 1].current.focus()
+                    }} />)
+            }
+
+        </div>
+        <p>El código expira en {}</p>
     </div>
 }
 export default ValidationComponent
---
+++ src/components/common/Input.js
@@ -0,0 +1,127 @@
+import * as React from 'react'
+
+const ERROR_REQUIRED = 'Campo requerido';
+const ERROR_NUMBER_FORMAT = 'Formato número incorrecto';
+const NUMBER_TYPE = 'number';
+const PASSWORD = 'password';
+const VALID_NUMBER_PATERN = /^\d+$/;
+
+const isValidNumber = (number) => {
+    return number !== '' && !VALID_NUMBER_PATERN.test(number);
+}
+
+const escapeRegExp = (str) =>
+    str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+
+export const keepOnlyNumberCharacters = (val = '', decimalSeparator = '.') => {
+    const numRegex = new RegExp(`\\d|${escapeRegExp(decimalSeparator)}`, 'g');
+    return (val.match(numRegex) || []).join('').replace(decimalSeparator, '.');
+};
+
+class Input extends React.Component {
+
+    static defaultProps = {
+        value: '',
+        required: false,
+        disabled: false,
+        autocomplete: 'nope',
+        minValue: null
+    };
+
+    onChange = (evt) => {
+        const target = evt.target
+        let value = target.value
+
+        value = this.maxLength(value)
+        const error = this.validateEmpty(value) || this.minValidation(value) || this.validateType(value) ||
+            this.validateMinValue(parseInt(value))
+        this.props.onChange(target.id, value, error)
+    }
+
+    onPressKey = (e) => {
+        const { onPressBack, onPressEnter, value } = this.props
+        if (e.keyCode === 13 && onPressEnter) {
+            onPressEnter();
+            return
+        }
+        if (e.keyCode === 8 && onPressBack && value === '') {
+            onPressBack();
+            return
+        }
+    }
+
+    minValidation = (value) => {
+        const minLength = this.props.minLength;
+        return minLength && value.length < minLength ? `mínimo ${minLength} caracteres` : null;
+    }
+
+    maxLength = (val) => {
+        const { type } = this.props;
+        const maxLength = this.props.maxLength || 100;
+        let value = val;
+        if (type === NUMBER_TYPE) {
+            value = keepOnlyNumberCharacters(value);
+        }
+        if (String(value).trim().length > maxLength) {
+            return value.substr(0, maxLength);
+        }
+        return value;
+    }
+
+    validateEmpty = (value) => this.props.required && value === '' ? ERROR_REQUIRED : null;
+
+    validateType = (value) => {
+        switch (this.props.type) {
+            case NUMBER_TYPE:
+                return isValidNumber(value) ? ERROR_NUMBER_FORMAT : null;
+            default:
+                return null;
+        }
+    }
+
+    validateMinValue = (value) => {
+        const minValue = this.props.minValue || 0;
+        return minValue != null && value <= minValue ? `Ingrese un valor mayor que ${minValue}` : null;
+    }
+
+    render() {
+        const {
+            id,
+            value,
+            required,
+            disabled,
+            minLength,
+            type,
+            autocomplete,
+            inputCustomClass,
+            readOnly,
+            inputReference
+        } = this.props;
+
+        const inputClasses = [
+            inputCustomClass
+        ].join(' ');
+
+        const inputType = type === PASSWORD ? PASSWORD : 'text';
+        const inputMode = type === NUMBER_TYPE ? 'numeric' : 'text';
+        return (
+            <input id={id} type={inputType} ref={inputReference ? inputReference : null}
+                className={inputClasses}
+                required={required}
+                disabled={disabled}
+                value={value ? value : value === 0 ? value : ''}
+                minLength={minLength}
+                autoComplete={autocomplete}
+                onKeyDown={this.onPressKey}
+                inputMode={inputMode}
+                readOnly={readOnly}
+                onChange={e =>
+                    this.props.onChange ?
+                        this.onChange(e) : {}
+                }
+                onClick={() => this.props.onClick ? this.props.onClick() : ''} />
+        );
+    }
+}
+
+export default Input;
